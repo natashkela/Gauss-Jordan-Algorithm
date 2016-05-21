906a041a452351a843fcbc1391fa0904468795b1
Nata Vacheishvili <nvacheishvili@wpi.edu>
2016-05-21 18:11:24 -0400

    Still has Errors

--- Gauss.java
+++ Gauss.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 public class Gauss{
-	
-    //-----------------------------------------
 	public static final double EPSILON = 1e-7;
     public final int num;
     public double[][] newNum;
